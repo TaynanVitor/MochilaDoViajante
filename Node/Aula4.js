edafaf89b69eae97772acfcc0177b1e54b929854
TaynanVitor <89108201+TaynanVitor@users.noreply.github.com>
2021-10-25 21:20:53 -0300

    commit
    Aula

---
+++ Node/Aula4.js
@@ -0,0 +1,29 @@
+console.log("Olá mundo!");
+var x = 7; // atribuição 
+var y = 6;
+let z = 2;
+
+let soma = x + y + z;
+let subtracao = x - y - z;
+let multiplicacao = x * y;
+let divisao = x / y;
+
+
+// console.log(soma);
+// console.log(subtracao);
+// console.log( multiplicacao);
+// console.log(divisao);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
